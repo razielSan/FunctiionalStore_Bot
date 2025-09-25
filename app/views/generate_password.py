d9740091f7faf7f0cf4c61de3c59c7987e0955cd
raziel <tvindzon@gmail.com>
2025-09-25 17:22:43 +0700

    create get_generating_password ( app\views\generate_password.py)

--- app/views/generate_password.py
+++ app/views/generate_password.py
@@ -3,6 +3,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import StateFilter
 
 from keyboards.inline_kb import get_buttons_for_generating_passwords
+from functions import get_generateing_simple_or_difficult_password
 
 
 router: Router = Router(name=__name__)
@@ -12,8 +13,19 @@ router: Router = Router(name=__name__)
 async def main(message: Message):
     """Возвращает пользователю инлайн клавиатуру с выбором вариантов генерации паролей."""
 
-    print("ok")
     await message.answer(
         text="Доступные варианты",
         reply_markup=get_buttons_for_generating_passwords(),
     )
+
+
+@router.callback_query(F.data.startswith("password "))
+async def get_generating_password(call: CallbackQuery):
+    """Возвращает сгенерированный пароль."""
+
+    _, password = call.data.split(" ")
+    passwords: str = get_generateing_simple_or_difficult_password(
+        step=3, password=password
+    )
+
+    await call.message.answer(text=passwords)
